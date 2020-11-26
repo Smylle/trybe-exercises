2e72476d4debd8b618be0c70b95e28b968aeecca
smylle <jjsilveira007@gmail.com>
2020-11-26 18:17:33 -0300

    Program Triangle Angles

---
+++ exercises/4.1/triangle-angles.js
@@ -0,0 +1,14 @@
+let angle1 = 60;
+let angle2 = 30;
+let angle3 = 90;
+
+result = true;
+sum = angle1 + angle2 +angle3;
+const triangle = 180;
+
+if (sum == triangle){
+    console.log(result);
+}
+else {
+    console.log(!result);
+}
