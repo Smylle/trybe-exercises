535fcd2fd59cb270ee4801c0e4eab0678deb277c
smylle <jjsilveira007@gmail.com>
2020-11-26 15:49:35 -0300

    Greater than three numbers

---
+++ exercises/4.1/greater3numbers.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 7;
+let c = 17
+
+if (a > b && a > c){
+    console.log('Este é o numero:', a );
+}
+else if(b > a && b > c){
+    console.log('Este é o numero:', b );
+}
+else if(c > a && c > b){
+    console.log('Este é o numero:', c );
+}
