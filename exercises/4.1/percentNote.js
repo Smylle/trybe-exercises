3b866029c4f82b639308480d32f519fd1c2a3529
smylle <jjsilveira007@gmail.com>
2020-11-27 09:00:44 -0300

    Files Pre-Prodution EXErc

---
+++ exercises/4.1/percentNote.js
@@ -0,0 +1,14 @@
+let note = 90;
+
+switch(note){
+    case (>= 90):
+        console.log("Conceito A");
+        break;
+
+    case (>=80):
+        console.log("Conceito B");
+        break; 
+
+    default:
+        console.log();
+}
