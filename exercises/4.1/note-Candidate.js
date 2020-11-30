58a17443648c829619bac243cb1ad296f7e3bcfb
smylle <jjsilveira007@gmail.com>
2020-11-30 18:30:05 -0300

    Note from candidate

---
+++ exercises/4.1/note-Candidate.js
@@ -0,0 +1,13 @@
+let note = 59;
+
+if (note >= 80) {
+    console.log('Parabéns, você foi aprovado');
+}
+
+else if (note < 80 && note >=60) {
+    console.log('Você está na lista de espera');
+}
+
+else {
+    console.log('Você foi reprovado');
+}
