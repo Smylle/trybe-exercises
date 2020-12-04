0593e1ebe43983f310352e5bb550b0fd087acb38
smylle <jjsilveira007@gmail.com>
2020-12-04 09:52:51 -0300

    Product custe

---
+++ exercises/4.1/ThreeNumbersDiferFalse.js
@@ -0,0 +1,11 @@
+let number1 = 6;
+let number2 = 8;
+let number3 = 10;
+
+if (number1 % 2 === 1 || number2 % 2 === 1 || number3 % 2 === 1) {
+    console.log('True');
+}
+
+else {
+    console.log('False');
+}
