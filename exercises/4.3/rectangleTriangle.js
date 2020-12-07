888d80941f625329fe3abe15884a168ef3d83fee
smylle <jjsilveira007@gmail.com>
2020-12-07 10:53:17 -0300

    Exercise 2 - rectangle Triangle

---
+++ exercises/4.3/rectangleTriangle.js
@@ -0,0 +1,12 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+
+let n = 5;
+let asterisk = '*';
+let inputLine = '';
+
+for (index = 0; index <= n; index += 1) {
+    console.log(inputLine);
+    inputLine = inputLine + asterisk;
+  }
+  
