82d08b3175fdf02b169c14127b13869c6c16f734
smylle <jjsilveira007@gmail.com>
2021-01-05 16:14:25 -0300

    building my forms

---
+++ exercises/6.1/script.js
@@ -0,0 +1,7 @@
+let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+// for (let index = 0; index < states.lenght; index += 1 ) {    
+    const selectState = document.querySelector("#states");
+    selectState.innerHTML = '<option>'states[0]'</option>';
+
+// }
