9419393f5143cb4de41905b38eadb95c2227b9b2
smylle <jjsilveira007@gmail.com>
2021-01-05 13:18:24 -0300

    Starting block 6 - flexbox

---
+++ exercises/4.4/retornaIndice.js
@@ -0,0 +1,9 @@
+function retornaIndice (numeros) {
+    for (let index = 0; index < numeros.length; index++) {
+        const element = numeros[index];
+    }
+    return numeros[index];
+
+}
+
+console.log(retornaIndice('2,4,5,6,7,21,29,30'));
