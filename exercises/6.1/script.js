f7dd50ee9e94be14d1e355bd8541dad2e509ceb4
smylle <jjsilveira007@gmail.com>
2021-01-05 23:28:16 -0300

    building my forms3

--- exercises/6.1/script.js
+++ exercises/6.1/script.js
@@ -1,7 +1,11 @@
+// estruturas de repetição para gerar os <option> dos estados do brasil.
 let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
-// for (let index = 0; index < states.lenght; index += 1 ) {    
-    const selectState = document.querySelector("#states");
-    selectState.innerHTML = '<option>'states[0]'</option>';
+const selectState = document.querySelector("#states");
 
-// }
+for (let index = 0; index < states.length; index += 1) {
+    let x = document.createElement('option');
+    selectState.appendChild(x).innerHTML = states[index];
+}
+// `<option> ${states[index]} </option>`;
+
