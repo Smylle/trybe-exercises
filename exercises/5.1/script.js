6617c41aac1fe0b2e69fc9565c142805fb933353
smylle <jjsilveira007@gmail.com>
2021-01-10 23:59:43 -0300

    exerc5.1 - administrador de tempo trybe

---
+++ exercises/5.1/script.js
@@ -0,0 +1,8 @@
+document.querySelector('h1').style.backgroundColor = 'green';
+
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'orange';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
