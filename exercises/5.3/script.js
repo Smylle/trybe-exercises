e23a4ebb9fdc1ea40db4f0fefc3424b82db88ae5
smylle <jjsilveira007@gmail.com>
2021-01-16 17:06:36 -0300

    Adiciona uma tarefa a lista de tarefas

--- exercises/5.3/script.js
+++ exercises/5.3/script.js
@@ -127,3 +127,14 @@ function dayMouseOver() {
   dayMouseOut();
 //   FIM Exercicio 6
 
+function newTaskSpan(task) {
+
+    let tasksContainer = document.querySelector('.my-tasks');
+    let taskName = document.createElement('span');
+  
+    taskName.innerHTML = task;
+    tasksContainer.appendChild(taskName);
+  };
+  
+  newTaskSpan('Projeto!');
+//   FIM Exercicio 7
