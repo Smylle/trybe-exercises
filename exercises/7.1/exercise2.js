3ddbfb9ac1084be029ababf988744a080ca23962
smylle <jjsilveira007@gmail.com>
2021-01-21 10:52:18 -0300

    exercise 2 - arrow function

--- exercises/7.1/exercise2.js
+++ exercises/7.1/exercise2.js
@@ -1,8 +1,6 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
-const sortoddsAndEvens = oddsAndEvens.sort(function(a, b) {
-    return a - b;
-});
+let sortoddsAndEvens = oddsAndEvens.sort((a, b) => a - b);
 
 console.log(`Este são os numeros ordenados ${sortoddsAndEvens}`);
