c83b40eec43a162a5dd6b4c8f14591a342dad035
smylle <jjsilveira007@gmail.com>
2021-01-21 15:23:26 -0300

    parte 2 exerc8

--- exercises/7.2/exercise2.js
+++ exercises/7.2/exercise2.js
@@ -48,3 +48,12 @@ const getNumberOfStudents = (obj) => {
   const getValueByNumber = (obj,number) => Object.values(obj)[number];
 console.log(getValueByNumber);
 
+const verifyPair = (obj, key, value) => {
+    const arr = Object.entries(obj);
+    let isEqual = false;
+    for (let i in arr) {
+      if (arr[i][0] === key && arr[i][1] === value) isEqual = true;
+    }
+    return isEqual;
+  };
+  console.log(verifyPair(lesson2,'professor','Carlos'));
